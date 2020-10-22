0f38e588513744db862942244109e64c636a781d
Cuong Pham <cuongpx.au@gmail.com>
2020-10-22 22:01:33 +1000

    Adding rotMNIST dataset for compare_all

--- compare_all.py
+++ compare_all.py
@@ -20,7 +20,7 @@ parser.add_argument('--results-dir', type=str, default='./results', dest='r_dir'
 
 # expirimental task parameters.
 task_params = parser.add_argument_group('Task Parameters')
-task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST'])
+task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['rotMNIST', 'permMNIST', 'splitMNIST'])
 task_params.add_argument('--scenario', type=str, default='task', choices=['task', 'domain', 'class'])
 task_params.add_argument('--tasks', type=int, help='number of tasks')
 
