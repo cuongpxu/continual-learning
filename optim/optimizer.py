16522af927382ab445005e39198439693507897d
Cuong Pham <cuongpx.au@gmail.com>
2020-12-24 10:02:36 +1000

    Update learning rate annealing method

---
+++ optim/optimizer.py
@@ -0,0 +1,262 @@
+import torch
+import math
+from torch.optim.optimizer import Optimizer
+
+
+class AdLR(Optimizer):
+    r"""Implements the algorithm proposed in the paper "A novel adaptive learning rate algorithm for CNN training"
+    Author: S.V. Georgakopoulos and V.P. Plagianakos
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate (required)
+        gamma1: meta-learning rate 1
+        gamma2: meta-learning rate 2
+        gamma3: meta-learning rate 3
+    """
+
+    def __init__(self, params, lr=0.01, gamma1=0.01, gamma2=0.001, gamma3=0.01):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr, gamma1=gamma1, gamma2=gamma2, gamma3=gamma3)
+        super(AdLR, self).__init__(params, defaults)
+        self.init_lr = lr
+
+        # Initialize previous grad
+        for k, p in enumerate(self.param_groups[0]['params']):
+            if k == 0:
+                self.prev_grad1 = torch.zeros(p.view(-1).size())
+            else:
+                self.prev_grad1 = torch.cat((self.prev_grad1, torch.zeros(p.view(-1).size())))
+
+        self.prev_grad2 = self.prev_grad1.clone()
+
+    def __setstate__(self, state):
+        super(AdLR, self).__setstate__(state)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """ Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for k, p in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+
+                grad = p.grad
+                if k == 0:
+                    all_grad = torch.flatten(p.grad)
+                else:
+                    all_grad = torch.cat((all_grad, torch.flatten(p.grad)))
+
+                # Update weight
+                p.add_(grad, alpha=-group['lr'])
+
+            # Update learning rate
+            group['lr'] = group['lr'] + \
+                              group['gamma1'] * torch.dot(self.prev_grad1, all_grad) + \
+                              group['gamma2'] * torch.dot(self.prev_grad2, self.prev_grad1)
+            if group['lr'] < 0:
+                group['lr'] = group['gamma3'] * self.init_lr
+
+            # Store prev grad
+            self.prev_grad2 = self.prev_grad1.clone()
+            self.prev_grad1 = all_grad.clone()
+
+        return loss
+
+
+class eAdLR(Optimizer):
+    r"""Implements the algorithm proposed in the paper "Efficient learning rate adaptation for CNN training"
+    Author: S.V. Georgakopoulos and V.P. Plagianakos
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate (required)
+        gamma1: meta-learning rate 1
+        gamma2: meta-learning rate 2
+        gamma3: meta-learning rate 3
+    """
+
+    def __init__(self, params, lr=0.01, gamma1=0.01, gamma2=0.001, gamma3=0.01):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr, gamma1=gamma1, gamma2=gamma2, gamma3=gamma3)
+        super(eAdLR, self).__init__(params, defaults)
+        self.init_lr = lr
+        self.iter = 0
+
+        # Initialize previous grad
+        self.prev_grad1 = dict({})
+        self.prev_grad2 = dict({})
+        for k, p in enumerate(self.param_groups[0]['params']):
+            self.prev_grad1[k] = torch.zeros(p.size())
+            self.prev_grad2[k] = torch.zeros(p.size())
+
+    def __setstate__(self, state):
+        super(eAdLR, self).__setstate__(state)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """ Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for k, p in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                grad = p.grad
+
+                # Update weight
+                p.add_(grad, alpha=-group['lr'])
+
+                # Update learning rate
+                if self.iter >= 3:
+                    group['lr'] = group['lr'] + \
+                                  group['gamma1'] * torch.dot(torch.flatten(self.prev_grad1[k]), torch.flatten(grad)) +\
+                                  group['gamma2'] * torch.dot(torch.flatten(self.prev_grad2[k]), torch.flatten(self.prev_grad1[k]))
+                    if group['lr'] < 0:
+                        group['lr'] = group['gamma3'] * self.init_lr
+
+                # Store prev grad
+                self.prev_grad2[k] = self.prev_grad1[k].clone()
+                self.prev_grad1[k] = grad.clone()
+
+        self.iter += 1
+        return loss
+
+
+class AdamAdLR(Optimizer):
+    r"""Integrating the algorithm proposed in the paper "Efficient learning rate adaptation for CNN training"
+    Author: S.V. Georgakopoulos and V.P. Plagianakos with ADAM optimizer
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate (required)
+        gamma1: meta-learning rate 1
+        gamma2: meta-learning rate 2
+        gamma3: meta-learning rate 3
+    """
+
+    def __init__(self, params, lr=0.01, gamma1=0.01, gamma2=0.001, gamma3=0.01,
+                 betas=(0.9, 0.999), eps=1e-8, weight_decay=0, amsgrad=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, gamma1=gamma1, gamma2=gamma2, gamma3=gamma3, betas=betas, eps=eps,
+                        weight_decay=weight_decay, amsgrad=amsgrad)
+        super(AdamAdLR, self).__init__(params, defaults)
+        self.init_lr = lr
+        self.iter = 0
+
+        # Initialize previous grad
+        self.prev_grad1 = dict({})
+        self.prev_grad2 = dict({})
+        for k, p in enumerate(self.param_groups[0]['params']):
+            self.prev_grad1[k] = torch.zeros(p.size())
+            self.prev_grad2[k] = torch.zeros(p.size())
+
+    def __setstate__(self, state):
+        super(AdamAdLR, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """ Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for k, p in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                grad = p.grad
+
+                # Update weight
+                if grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                amsgrad = group['amsgrad']
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    if amsgrad:
+                        # Maintains max of all exp. moving avg. of sq. grad. values
+                        state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                if amsgrad:
+                    max_exp_avg_sq = state['max_exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(p, alpha=group['weight_decay'])
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+                if amsgrad:
+                    # Maintains the maximum of all 2nd moment running avg. till now
+                    torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                    # Use the max. for normalizing running avg. of gradient
+                    denom = (max_exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+                else:
+                    denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+
+                step_size = group['lr'] / bias_correction1
+
+                p.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # Update learning rate
+                if self.iter >= 3:
+                    group['lr'] = group['lr'] + \
+                                  group['gamma1'] * torch.dot(torch.flatten(self.prev_grad1[k]), torch.flatten(grad)) +\
+                                  group['gamma2'] * torch.dot(torch.flatten(self.prev_grad2[k]), torch.flatten(self.prev_grad1[k]))
+                    if group['lr'] < 0:
+                        group['lr'] = group['gamma3'] * self.init_lr
+
+                # Store prev grad
+                self.prev_grad2[k] = self.prev_grad1[k].clone()
+                self.prev_grad1[k] = grad.clone()
+
+        self.iter += 1
+        return loss
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from continual_learner import ContinualLearner
 from es import EarlyStopping
 from torch import optim
 from torch.utils.data import ConcatDataset, random_split
+from optim import optimizer
 import uuid
 
 
@@ -417,8 +418,8 @@ def training_teacher(teacher_dataset, teacher, teacher_lr, batch_size, cuda):
                                              shuffle=True, drop_last=False, cuda=cuda)
     mem_val_loader = utils.get_data_loader(mem_val_set, batch_size=batch_size,
                                            shuffle=False, drop_last=False, cuda=cuda)
-    teacher_optimizer = optim.Adam(teacher.parameters(), lr=teacher_lr, betas=(0.9, 0.999))
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(teacher_optimizer, 'min', patience=3)
+    teacher_optimizer = optimizer.eAdLR(teacher.parameters(), lr=teacher_lr)
+
     teacher_criterion = torch.nn.CrossEntropyLoss()
     id = uuid.uuid1()
     early_stopping = EarlyStopping(model_name=id.hex, verbose=False)
@@ -430,7 +431,7 @@ def training_teacher(teacher_dataset, teacher, teacher_lr, batch_size, cuda):
     for epoch in tk:
         teacher.train_epoch(mem_train_loader, teacher_criterion, teacher_optimizer, epoch)
         vlosses = teacher.valid_epoch(mem_val_loader, teacher_criterion)
-        scheduler.step(np.average(vlosses))
+        # scheduler.step(np.average(vlosses))
         early_stopping(np.average(vlosses), teacher, epoch)
         if early_stopping.early_stop:
             # print("Teacher early stopping detected")
