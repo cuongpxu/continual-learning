afa8594788933f4874a286f18dcc8cc6663ae9e9
Cuong Pham <cuongpx.au@gmail.com>
2021-01-16 21:32:16 +1000

    Remove batch mean

--- encoder.py
+++ encoder.py
@@ -390,22 +390,22 @@ class Classifier(ContinualLearner, Replayer, ExemplarHandler):
 
                     if params_dict['distill_type'] in ['ET', 'ETS']:
                         loss_KD = 0.5 * (F.kl_div(F.log_softmax(y_hat / self.KD_temp, dim=1),
-                                                  F.softmax(y_hat_ensemble / self.KD_temp, dim=1), reduction='batchmean')
+                                                  F.softmax(y_hat_ensemble / self.KD_temp, dim=1))
                                          * (self.KD_temp * self.KD_temp) +
                                          F.kl_div(F.log_softmax(y_hat / self.KD_temp, dim=1),
-                                                  F.softmax(y_hat_teacher.detach() / self.KD_temp, dim=1), reduction='batchmean')
+                                                  F.softmax(y_hat_teacher.detach() / self.KD_temp, dim=1))
                                          * (self.KD_temp * self.KD_temp))
 
                     else: # distill: E, ES
                         loss_KD = F.kl_div(F.log_softmax(y_hat / self.KD_temp, dim=1),
-                                           F.softmax(y_hat_ensemble / self.KD_temp, dim=1), reduction='batchmean') \
+                                           F.softmax(y_hat_ensemble / self.KD_temp, dim=1)) \
                                    * (self.KD_temp * self.KD_temp)
 
                 else: # distill: T, TS
                     loss_KD = F.kl_div(F.log_softmax(y_hat / self.KD_temp, dim=1),
-                                       F.softmax(y_hat_teacher.detach() / self.KD_temp, dim=1), reduction='batchmean') \
+                                       F.softmax(y_hat_teacher.detach() / self.KD_temp, dim=1)) \
                                    * (self.alpha_t * self.KD_temp * self.KD_temp)
-                loss_KD = self.alpha_t * loss_KD + F.cross_entropy(y_hat, y) * (1. - self.alpha_t)
+                # loss_KD = self.alpha_t * loss_KD + F.cross_entropy(y_hat, y) * (1. - self.alpha_t)
             else:
                 loss_KD = None
 
@@ -564,25 +564,24 @@ class Classifier(ContinualLearner, Replayer, ExemplarHandler):
         if distill_type == 'T':
             return
         self.train()
-
+        self.optimizer.zero_grad()
         if distill_type in ['E', 'ET', 'ES', 'ETS']:
             with torch.no_grad():
                 y_hat_ensemble = 0.5 * (y_hat_teacher + y_hat)
             if distill_type in ['ES', 'ETS']: # distill from ensemble and student to teacher
                 loss = 0.5 * (F.kl_div(F.log_softmax(y_hat_teacher / self.KD_temp, dim=1),
-                                       F.softmax(y_hat_ensemble / self.KD_temp, dim=1), reduction='batchmean')
+                                       F.softmax(y_hat_ensemble / self.KD_temp, dim=1))
                               * (self.KD_temp * self.KD_temp) +
                               F.kl_div(F.log_softmax(y_hat_teacher / self.KD_temp, dim=1),
-                                       F.softmax(y_hat / self.KD_temp, dim=1), reduction='batchmean')
+                                       F.softmax(y_hat / self.KD_temp, dim=1))
                               * (self.KD_temp * self.KD_temp))
             else: # distill from ensemble to teacher
                 loss = F.kl_div(F.log_softmax(y_hat_teacher / self.KD_temp, dim=1),
-                                F.softmax(y_hat_ensemble / self.KD_temp, dim=1), reduction='batchmean') \
+                                F.softmax(y_hat_ensemble / self.KD_temp, dim=1)) \
                        * (self.KD_temp * self.KD_temp)
         else:
             loss = F.kl_div(F.log_softmax(y_hat_teacher / self.KD_temp, dim=1),
-                            F.softmax(y_hat / self.KD_temp, dim=1), reduction='batchmean') \
+                            F.softmax(y_hat / self.KD_temp, dim=1)) \
                    * (self.KD_temp * self.KD_temp)
-        self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
