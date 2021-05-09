244928e74e909a9d9f262155d247298ae08fad47
Cuong Pham <cuongpx.au@gmail.com>
2021-05-09 10:53:46 +1000

    Update normalize for CIFAR dataset

--- data.py
+++ data.py
@@ -338,7 +338,7 @@ AVAILABLE_TRANSFORMS = {
         transforms.ToTensor(),
     ],
     'cub2011_norm': [
-            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ],
     'cub2011_denorm': UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     'augment': [
